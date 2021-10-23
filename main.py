39bbcc043692cedb5c02fa361d514f49ccb78823
Flandre Scarlet <i@flandrescarlet.cn>
2021-10-23 20:42:38 +0800

    添加try防止被奇奇怪怪的bug卡住

--- main.py
+++ main.py
@@ -20,20 +20,19 @@ from notice import Notice
 
 # ===========================================================
 # Github actions 使用，本地使用请注释
-try:
-    config.account[0]["mobile"] = os.environ["YB_MOBILE"]
-    config.account[0]["password"] = os.environ["YB_PASSWORD"]
-    config.account[0]["mail"] = os.environ["YB_MAIL"]
-    config.account[0]["pushToken"] = os.environ["YB_PUSHTOKEN"]
-    config.account[0]["notice"] = os.environ["YB_NOTICE"]
-except KeyError:
-    pass
-# # ===========================================================
+# try:
+#     config.account[0]["mobile"] = os.environ["YB_MOBILE"]
+#     config.account[0]["password"] = os.environ["YB_PASSWORD"]
+#     config.account[0]["mail"] = os.environ["YB_MAIL"]
+#     config.account[0]["pushToken"] = os.environ["YB_PUSHTOKEN"]
+#     config.account[0]["notice"] = os.environ["YB_NOTICE"]
+# except KeyError:
+#     pass
+# # # ===========================================================
 
 for ac in config.account:
     yb = Yiban(ac.get("mobile"), ac.get("password"))
-    # 获取签到时段数值
-    nowPeriod = util.getTimePeriod()
+    nowPeriod = util.getTimePeriod()  # 获取签到时段数值
 
     if nowPeriod != 0:
         login = yb.login()
@@ -43,152 +42,161 @@ for ac in config.account:
 
         else:
             notice = Notice(config.admin, ac)
-            auth = yb.auth()
 
-            if auth["code"] == 0:
-                timePeriod = util.fromIntGetTimePeriod(nowPeriod)
-                now_task = yb.getUncompletedListTime(timePeriod[0], timePeriod[1])
+            try:
+                auth = yb.auth()
 
-                if not len(now_task["data"]):
-                    print("没有找到要提交的表单")
+                if auth["code"] == 0:
+                    timePeriod = util.fromIntGetTimePeriod(nowPeriod)
+                    now_task = yb.getUncompletedListTime(timePeriod[0], timePeriod[1])
+
+                    if not len(now_task["data"]):
+                        print("没有找到要提交的表单")
+
+                    else:
+                        now_task_id = now_task["data"][0]["TaskId"]
+                        detail = yb.getDetail(now_task_id)
+                        extend = {
+                            "TaskId": now_task_id,
+                            "title": "任务信息",
+                            "content": [
+                                {"label": "任务名称", "value": detail["data"]["Title"]},
+                                {"label": "发布机构", "value": detail["data"]["PubOrgName"]},
+                                {"label": "发布人", "value": detail["data"]["PubPersonName"]}
+                            ]
+                        }
+                        sb_result = yb.submitApply(config.tasks[nowPeriod - 1], extend)
+
+                        if nowPeriod != 3:
+
+                            if sb_result["code"] == 0:
+                                nick = ac.get("nick")
+                                sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                                address = json.loads(config.address)["Address"]
+                                result = {
+	                                "nick": nick,
+	                                "status": "表单签到成功",
+	                                "sgin_time": sgin_time,
+                                    "address": address
+                                    }
+                                notice.send(json.dumps(result,ensure_ascii=False))
+                                # 设置间隔时间
+                                time.sleep(3)
+                                # result = "{\"nick\" : " + nick + "\"status\" : \"提交成功\",\"sgin_time\" : " + sgin_time + "\"address\" : " + address + "\"sgin_URL\" : " + share_url + "}"
+                                # notice.send(result)
+
+                            else:
+                                nick = ac.get("nick")
+                                sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                                address = json.loads(config.address)["Address"]
+                                result = {
+	                                "nick": nick,
+	                                "status": "表单签到失败",
+	                                "sgin_time": sgin_time,
+                                    "address": address
+                                    }
+                                notice.send(json.dumps(result,ensure_ascii=False))
+                                # result = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())) + "表单提交失败！请检查\n")
+                                # notice.send(time.strftime(result))
+                                Notice.log(str(result))
+                                # 设置间隔时间
+                                time.sleep(3)
+                        else:
+                            # 位置签到
+                            yb.photoRequirements()
+                            yb.deviceState()
+                            yb.signPostion()
+                            ns_result = yb.nightAttendance(config.address)
+
+                            if sb_result["code"] == 0 and ns_result["code"] == 0:
+                                nick = ac.get("nick")
+                                sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                                address = json.loads(config.address)["Address"]
+                                result = {
+                                    "nick": nick,
+                                    "status": "表单和位置签到成功",
+                                    "sgin_time": sgin_time,
+                                    "address": address
+                                    }
+                                notice.send(json.dumps(result,ensure_ascii=False))
+                                # 设置间隔时间
+                                time.sleep(3)
+
+                                # result = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))) + " 表单和位置签到提交成功 url: " + share_url + " 位置: " + json.loads(config.address)["Address"] + "\n"
+                                # notice.send(result)
+
+                            elif sb_result["code"] == 0 and ns_result["code"] != 0:
+                                # print(str(sb_result["data"]))
+                                nick = ac.get("nick")
+                                sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                                address = json.loads(config.address)["Address"]
+                                result = {
+                                    "nick": nick,
+                                    "status": "表单签到成功，位置签到失败",
+                                    "sgin_time": sgin_time,
+                                    "address": address
+                                    }
+                                notice.send(json.dumps(result,ensure_ascii=False))
+                                # 设置间隔时间
+                                time.sleep(3)
+
+                                # result = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))) +" 表单提交成功 url: " + share_url + "\n"
+                                # notice.send(result)
+
+                            elif sb_result["code"] != 0 and ns_result["code"] == 0:
+                                nick = ac.get("nick")
+                                sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                                address = json.loads(config.address)["Address"]
+                                result = {
+                                    "nick": nick,
+                                    "status": "表单签到失败，位置签到成功",
+                                    "sgin_time": sgin_time,
+                                    "address": address
+                                    }
+                                notice.send(json.dumps(result,ensure_ascii=False))
+                                # 设置间隔时间
+                                time.sleep(3)
+                                # result = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))) +" 位置: " + json.loads(config.address)["Address"] + "\n"
+                                # notice.send(result)
+
+                            else:
+                                nick = ac.get("nick")
+                                sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                                address = json.loads(config.address)["Address"]
+                                result = {
+                                    "nick": nick,
+                                    "status": "表单和位置签到失败",
+                                    "sgin_time": sgin_time,
+                                    "address": address
+                                    }
+                                notice.send(json.dumps(result,ensure_ascii=False))
+                                # 设置间隔时间
+                                time.sleep(3)
+                                # result = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(int(time.time())) + "签到失败，请检查\n")
+                                # notice.send(result)
 
                 else:
-                    now_task_id = now_task["data"][0]["TaskId"]
-                    detail = yb.getDetail(now_task_id)
-                    extend = {
-                        "TaskId": now_task_id,
-                        "title": "任务信息",
-                        "content": [
-                            {"label": "任务名称", "value": detail["data"]["Title"]},
-                            {"label": "发布机构", "value": detail["data"]["PubOrgName"]},
-                            {"label": "发布人", "value": detail["data"]["PubPersonName"]}
-                        ]
-                    }
-                    sb_result = yb.submitApply(config.tasks[nowPeriod - 1], extend)
+                    print("登录授权失败，请重新登录!")
+                    nick = ac.get("nick")
+                    sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                    address = json.loads(config.address)["Address"]
+                    result = {
+                        "nick": nick,
+                        "status": "登录授权失败",
+                        "sgin_time": sgin_time,
+                        "address": address
+                        }
+                    notice.send(json.dumps(result,ensure_ascii=False))
+                    # 设置间隔时间
+                    time.sleep(3)
+                    # notice.send(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(int(time.time())) + "登录授权失败，请重新登录\n"))
 
-                    if nowPeriod != 3:
-
-                        if sb_result["code"] == 0:
-                            nick = ac.get("nick")
-                            sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
-                            address = json.loads(config.address)["Address"]
-                            result = {
-	                            "nick": nick,
-	                            "status": "表单签到成功",
-	                            "sgin_time": sgin_time,
-                                "address": address
-                                }
-                            notice.send(json.dumps(result,ensure_ascii=False))
-                            # 设置间隔时间
-                            time.sleep(3)
-                            # result = "{\"nick\" : " + nick + "\"status\" : \"提交成功\",\"sgin_time\" : " + sgin_time + "\"address\" : " + address + "\"sgin_URL\" : " + share_url + "}"
-                            # notice.send(result)
-
-                        else:
-                            nick = ac.get("nick")
-                            sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
-                            address = json.loads(config.address)["Address"]
-                            result = {
-	                            "nick": nick,
-	                            "status": "表单签到失败",
-	                            "sgin_time": sgin_time,
-                                "address": address
-                                }
-                            notice.send(json.dumps(result,ensure_ascii=False))
-                            # result = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())) + "表单提交失败！请检查\n")
-                            # notice.send(time.strftime(result))
-                            Notice.log(str(result))
-                            # 设置间隔时间
-                            time.sleep(3)
-                    else:
-                        # 位置签到
-                        yb.photoRequirements()
-                        yb.deviceState()
-                        yb.signPostion()
-                        ns_result = yb.nightAttendance(config.address)
-
-                        if sb_result["code"] == 0 and ns_result["code"] == 0:
-                            nick = ac.get("nick")
-                            sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
-                            address = json.loads(config.address)["Address"]
-                            result = {
-	                            "nick": nick,
-	                            "status": "表单和位置签到成功",
-	                            "sgin_time": sgin_time,
-                                "address": address
-                                }
-                            notice.send(json.dumps(result,ensure_ascii=False))
-                            # 设置间隔时间
-                            time.sleep(3)
-
-                            # result = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))) + " 表单和位置签到提交成功 url: " + share_url + " 位置: " + json.loads(config.address)["Address"] + "\n"
-                            # notice.send(result)
-
-                        elif sb_result["code"] == 0 and ns_result["code"] != 0:
-                            print(str(sb_result["data"]))
-                            nick = ac.get("nick")
-                            sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
-                            address = json.loads(config.address)["Address"]
-                            result = {
-	                            "nick": nick,
-	                            "status": "表单签到成功，位置签到失败",
-	                            "sgin_time": sgin_time,
-                                "address": address
-                                }
-                            notice.send(json.dumps(result,ensure_ascii=False))
-                            # 设置间隔时间
-                            time.sleep(3)
-
-                            # result = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))) +" 表单提交成功 url: " + share_url + "\n"
-                            # notice.send(result)
-
-                        elif sb_result["code"] != 0 and ns_result["code"] == 0:
-                            nick = ac.get("nick")
-                            sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
-                            address = json.loads(config.address)["Address"]
-                            result = {
-	                            "nick": nick,
-	                            "status": "表单签到失败，位置签到成功",
-	                            "sgin_time": sgin_time,
-                                "address": address
-                                }
-                            notice.send(json.dumps(result,ensure_ascii=False))
-                            # 设置间隔时间
-                            time.sleep(3)
-                            # result = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time()))) +" 位置: " + json.loads(config.address)["Address"] + "\n"
-                            # notice.send(result)
-
-                        else:
-                            nick = ac.get("nick")
-                            sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
-                            address = json.loads(config.address)["Address"]
-                            result = {
-	                            "nick": nick,
-	                            "status": "表单和位置签到失败",
-	                            "sgin_time": sgin_time,
-                                "address": address
-                                }
-                            notice.send(json.dumps(result,ensure_ascii=False))
-                            # 设置间隔时间
-                            time.sleep(3)
-                            # result = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(int(time.time())) + "签到失败，请检查\n")
-                            # notice.send(result)
-
-            else:
-                print("登录授权失败，请重新登录!")
+            except Exception as e:
                 nick = ac.get("nick")
-                sgin_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
-                address = json.loads(config.address)["Address"]
-                result = {
-	                "nick": nick,
-	                "status": "登录授权失败",
-	                "sgin_time": sgin_time,
-                    "address": address
-                    }
-                notice.send(json.dumps(result,ensure_ascii=False))
-                # 设置间隔时间
-                time.sleep(3)
-                # notice.send(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(int(time.time())) + "登录授权失败，请重新登录\n"))
-
+                status = nick + "状态异常！请检查易班校本化授权！" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+                print(status)
+                print(e)
+                Notice.saveLocal(str(e + status))
+            
     else:
         print("未到签到时间")
